1dcc38a14848ba56faf5da627d0f1fef46d432c4
Emanuel <123523520+emanueldias01@users.noreply.github.com>
2024-06-11 13:28:31 -0300

    desabilitando csrf

---
+++ ecommerce/src/main/java/br/com/emanuel/ecommerce/security/SecurityConfig.java
@@ -0,0 +1,21 @@
+package br.com.emanuel.ecommerce.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> {
+                   auth.anyRequest().permitAll();
+                }).build();
+    }
+}
