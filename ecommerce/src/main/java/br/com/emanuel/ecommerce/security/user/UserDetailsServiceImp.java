080b2b92e45f6252c46efe39c4b0d7173041e377
Emanuel <123523520+emanueldias01@users.noreply.github.com>
2024-06-11 13:44:19 -0300

    criacao do ImpService de user

---
+++ ecommerce/src/main/java/br/com/emanuel/ecommerce/security/user/UserDetailsServiceImp.java
@@ -0,0 +1,16 @@
+package br.com.emanuel.ecommerce.security.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserDetailsServiceImp implements UserDetailsService {
+    @Autowired
+    UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return repository.findByUsername(username);
+    }
+}
---
+++ ecommerce/src/main/java/br/com/emanuel/ecommerce/security/user/UserRepository.java
@@ -0,0 +1,10 @@
+package br.com.emanuel.ecommerce.security.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+    UserDetails findByUsername(String username);
+}
