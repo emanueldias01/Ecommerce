c8ea6b097325305700431108e9da303291f4cd4e
Emanuel <123523520+emanueldias01@users.noreply.github.com>
2024-06-12 09:16:15 -0300

    criacao do token service

---
+++ ecommerce/src/main/java/br/com/emanuel/ecommerce/security/token/TokenService.java
@@ -0,0 +1,52 @@
+package br.com.emanuel.ecommerce.security.token;
+
+import br.com.emanuel.ecommerce.security.user.User;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class TokenService {
+
+    @Value("${api.key.secret.security}")
+    private String secret;
+
+    public String generateToken(User user){
+        Algorithm algorithm = Algorithm.HMAC256(secret);
+
+        try{
+            var token = JWT.create()
+                    .withIssuer("ecommerce-api")
+                    .withSubject(user.getUsername())
+                    .withExpiresAt(generateTimeValidToken())
+                    .sign(algorithm);
+            return token;
+        } catch (JWTCreationException ex){
+            throw new RuntimeException("ERROR: " + ex.getMessage());
+        }
+    }
+
+    public String validateToken(String token){
+        Algorithm algorithm = Algorithm.HMAC256(secret);
+        try{
+            return JWT.require(algorithm)
+                    .withIssuer("ecommerce-api")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        } catch (JWTVerificationException ex){
+            return "";
+        }
+    }
+
+    private Instant generateTimeValidToken(){
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
+}
