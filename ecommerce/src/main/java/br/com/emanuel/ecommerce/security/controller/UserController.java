dd7f7c64c25e0983bbd020d5c31f7eebd06942f7
Emanuel <123523520+emanueldias01@users.noreply.github.com>
2024-06-20 10:36:44 -0300

    refatoracao do UserController

--- ecommerce/src/main/java/br/com/emanuel/ecommerce/security/controller/UserController.java
+++ ecommerce/src/main/java/br/com/emanuel/ecommerce/security/controller/UserController.java
@@ -7,6 +7,7 @@ import br.com.emanuel.ecommerce.exceptions.UsuarioJaExisteException;
 import br.com.emanuel.ecommerce.security.dto.LoginRequestDTO;
 import br.com.emanuel.ecommerce.security.dto.RegisterDTO;
 import br.com.emanuel.ecommerce.security.dto.TokenDTO;
+import br.com.emanuel.ecommerce.security.service.UserService;
 import br.com.emanuel.ecommerce.security.token.TokenService;
 import br.com.emanuel.ecommerce.security.user.User;
 import br.com.emanuel.ecommerce.security.user.UserRepository;
@@ -23,67 +24,31 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/user")
 public class UserController {
-
     @Autowired
-    AuthenticationManager authenticationManager;
-
-    @Autowired
-    UserRepository repository;
-
-    @Autowired
-    TokenService tokenService;
-
-    @Autowired
-    EmailRegisterService emailRegisterService;
+    UserService service;
 
     @PostMapping("/login")
     public ResponseEntity login(@RequestBody LoginRequestDTO dto) {
-        var usernamePassword = new UsernamePasswordAuthenticationToken(dto.username(), dto.password());
-        var auth = authenticationManager.authenticate(usernamePassword);
-
-        var token = tokenService.generateToken((User) auth.getPrincipal());
-
+        var token = service.loginService(dto);
         return ResponseEntity.ok(new TokenDTO(token));
     }
 
 
     @PostMapping("/verifyEmail")
     public ResponseEntity verifyEmail(@RequestBody EmailRegisterDTO dto) {
-
-        if (repository.findByEmail(dto.email()) != null) {
-            throw new UsuarioJaExisteException("Já existe alguém com esse EMAIL registrado");
-        }
-
-        emailRegisterService.sendEmailRegister((new Email(dto.email())));
-
+        service.verifyEmailService(dto);
         return ResponseEntity.ok().build();
     }
 
     @PostMapping("/verifyTokenEmail")
     public ResponseEntity verifyTokenEmail(@RequestBody TokenDTO dto){
-
-        tokenService.validateToken(dto.token());
-
+        service.validateEmailTokenService(dto);
         return ResponseEntity.ok().build();
     }
 
     @PostMapping("/finalRegister")
     public ResponseEntity finalRegister(@RequestBody RegisterDTO dto) {
-
-        if (repository.findByUsername(dto.username()) != null) {
-            throw new UsuarioJaExisteException("Já existe alguém com esse USERNAME registrado");
-
-        }
-        User userSave = new User();
-        String passwordEncrypt = new BCryptPasswordEncoder().encode(dto.password());
-
-        userSave.setUsername(dto.username());
-        userSave.setEmail(dto.email());
-        userSave.setPassword(passwordEncrypt);
-        userSave.setRole(dto.role());
-
-        repository.save(userSave);
-
+        service.finalRegisterUserService(dto);
         return ResponseEntity.ok().build();
     }
 }
