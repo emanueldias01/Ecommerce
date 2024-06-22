bd4130cb3e7fd2b9ac1c61f741dd8f9ee44959f4
Emanuel <123523520+emanueldias01@users.noreply.github.com>
2024-06-22 09:39:39 -0300

    enum em string

--- ecommerce/src/main/java/br/com/emanuel/ecommerce/security/user/User.java
+++ ecommerce/src/main/java/br/com/emanuel/ecommerce/security/user/User.java
@@ -36,6 +36,7 @@ public class User implements UserDetails {
     private String password;
 
     @Column(name = "role")
+    @Enumerated(EnumType.STRING)
     private UserRole role;
 
     public UUID getId() {
